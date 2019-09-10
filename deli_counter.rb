8f6022d6c650695999e0bc7b558b7d1b0042824b
ineffable-key-9064 <cristianlzambrano@outlook.com>
2019-09-10 02:29:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,12 +4,12 @@ def line(ln)
   if (ln.empty?)
     return "The line is currently empty."
   
-  return line
+  # return line
 end
 
 def take_a_number(array, new_customer)
   puts "Welcome, #{new_customer}. You are number #{array.lenght} in line."
-  array.push(new_customer)
+  # array.push(new_customer)
 end
 
 def now_serving(array)
@@ -17,5 +17,5 @@ def now_serving(array)
     return "There is nobody waiting to be served!"
   
   puts "now serving #{array}"
-  array.pop
+  # array.pop
 end
