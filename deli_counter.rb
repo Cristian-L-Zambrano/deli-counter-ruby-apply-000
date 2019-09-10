9d52b72fb1dd00ce63b5148c8a67466922430f36
ineffable-key-9064 <cristianlzambrano@outlook.com>
2019-09-10 02:19:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,6 +2,12 @@
 
 def line(ln)
   if (ln.empty)
-    return "the line is curre"
+    return "The line is currently empty."
   
+  return line
+end
+
+def take_a_number(array, new_customer)
+  puts new_customer
+  array.push(new_customer)
 end
