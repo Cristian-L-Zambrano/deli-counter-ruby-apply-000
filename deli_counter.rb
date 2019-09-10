5cfba050e760221b7305acae84560074778e0536
ineffable-key-9064 <cristianlzambrano@outlook.com>
2019-09-10 02:29:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,21 +1,21 @@
 # Write your code here.
 
 def line(ln)
-  if (ln.empty?)
-    return "The line is currently empty."
+  #if (ln.empty?)
+  #  return "The line is currently empty."
   
   # return line
 end
 
 def take_a_number(array, new_customer)
-  puts "Welcome, #{new_customer}. You are number #{array.lenght} in line."
+  #puts "Welcome, #{new_customer}. You are number #{array.lenght} in line."
   # array.push(new_customer)
 end
 
 def now_serving(array)
-  if (array.empty?)
-    return "There is nobody waiting to be served!"
+  #if (array.empty?)
+   # return "There is nobody waiting to be served!"
   
-  puts "now serving #{array}"
-  array.pop
+  #puts "now serving #{array}"
+  #array.pop
 end
